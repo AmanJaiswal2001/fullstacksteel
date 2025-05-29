202daeb4236d2f4b6a4c6a00b384e6838682b0ec
AmanJaiswal2001 <amanjaisawal537@gmail.com>
2025-05-29 16:09:59 +0530

    up

--- backend/index.js
+++ backend/index.js
@@ -49,9 +49,9 @@ app.use("/api",contactRoutes);
 app.use('/api/admin',blogRoutes);
 
 app.use(express.static(path.join(__dirname, 'dist')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 
 
